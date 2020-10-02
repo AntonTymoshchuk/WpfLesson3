7821907b14c592d74d9b98779397313c76af94cd
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:11:10 +0300

    Add project files.

---
+++ WpfLesson3/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfLesson3
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+        }
+    }
+}
